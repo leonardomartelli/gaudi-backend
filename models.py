671282eef36ed7faf9834ba22e37d8a96f05112b
leonardomartelli <leonardo@martellioliveira.me>
2023-08-15 20:56:15 -0300

    moving optimization to models.py

--- models.py
+++ models.py
@@ -5,11 +5,13 @@ import numpy
 from topopt.boundary_conditions import BoundaryConditions as bc
 from topopt.filters import Filter
 from topopt.guis import GUI
-from topopt.problems import Problem
+from topopt.problems import Problem, ComplianceProblem
 from topopt.utils import xy_to_id
 from topopt.solvers import TopOptSolver
-import numpy as np
+from topopt.filters import DensityBasedFilter
+
 from queue import SimpleQueue
+import time
 
 
 class RegionType(Enum):
@@ -214,25 +216,25 @@ class CustomBoundaryConditions(bc):
         for support in self.boundary_conditions.supports:
             if support.size > 0:
                 if support.orientation == 0:
-                    func = np.vectorize(lambda x: xy_to_id(
+                    func = numpy.vectorize(lambda x: xy_to_id(
                         x, support.position.y, self.nelx, self.nely))
                     begin = support.position.x
                 else:
-                    func = np.vectorize(lambda y: xy_to_id(
+                    func = numpy.vectorize(lambda y: xy_to_id(
                         support.position.x, y, self.nelx, self.nely))
                     begin = support.position.y
 
                 ids = 2 * func(range(begin, begin + support.size))
 
-                supp_points = np.union1d(ids, ids + 1)
+                supp_points = numpy.union1d(ids, ids + 1)
             else:
                 index = 2 * xy_to_id(support.position.x,
                                      support.position.y, self.nelx, self.nely)
 
-                supp_points = np.arange(index, index+1 + support.type)
+                supp_points = numpy.arange(index, index+1 + support.type)
 
             if fixed is not None:
-                fixed = np.union1d(fixed, supp_points)
+                fixed = numpy.union1d(fixed, supp_points)
             else:
                 fixed = supp_points
 
@@ -243,18 +245,18 @@ class CustomBoundaryConditions(bc):
 
         forces_length = len(self.boundary_conditions.forces)
 
-        f = np.zeros((self.ndof, forces_length))
+        f = numpy.zeros((self.ndof, forces_length))
 
         for i in range(forces_length):
             force = self.boundary_conditions.forces[i]
 
             if force.size > 0:
                 if force.orientation == 0:
-                    func = np.vectorize(lambda x: xy_to_id(
+                    func = numpy.vectorize(lambda x: xy_to_id(
                         x, force.position.y, self.nelx, self.nely))
                     begin = force.position.x
                 else:
-                    func = np.vectorize(lambda y: xy_to_id(
+                    func = numpy.vectorize(lambda y: xy_to_id(
                         force.position.x, y, self.nelx, self.nely))
                     begin = force.position.y
                 ids = func(range(begin, begin + force.size))
@@ -263,7 +265,7 @@ class CustomBoundaryConditions(bc):
             else:
                 index = 2 * xy_to_id(force.position.x,
                                      force.position.y, self.nelx, self.nely) + force.orientation
-                ids = np.arange(index, index + 1)
+                ids = numpy.arange(index, index + 1)
 
             f[ids, i] = force.load
 
@@ -284,18 +286,18 @@ class CustomBoundaryConditions(bc):
             if region.type != region_type:
                 continue
 
-            X_t, Y_t = np.mgrid[region.position.x: region.position.x + region.dimensions.width + 1,
-                                region.position.y: region.position.y + region.dimensions.height + 1]
+            X_t, Y_t = numpy.mgrid[region.position.x: region.position.x + region.dimensions.width + 1,
+                                   region.position.y: region.position.y + region.dimensions.height + 1]
 
             if X is None:
                 X, Y = X_t.ravel(), Y_t.ravel()
             else:
-                X = np.append(X.ravel(), X_t.ravel())
-                Y = np.append(Y.ravel(), Y_t.ravel())
+                X = numpy.append(X.ravel(), X_t.ravel())
+                Y = numpy.append(Y.ravel(), Y_t.ravel())
 
-        pairs = np.vstack([X.ravel(), Y.ravel()]).T
+        pairs = numpy.vstack([X.ravel(), Y.ravel()]).T
 
-        to_id = np.vectorize(lambda xy: xy_to_id(
+        to_id = numpy.vectorize(lambda xy: xy_to_id(
             *xy, nelx=self.nelx - 1, nely=self.nely - 1), signature="(m)->()")
 
         return to_id(pairs)
@@ -325,7 +327,7 @@ class GaudiSolver(TopOptSolver):
         super().__init__(problem, volfrac, filter, gui, maxeval, ftol_rel)
         self.results = SimpleQueue()
 
-    def objective_function(self, x: np.ndarray, dobj: np.ndarray) -> float:
+    def objective_function(self, x: numpy.ndarray, dobj: numpy.ndarray) -> float:
 
         obj = super().objective_function(x, dobj)
 
@@ -338,7 +340,7 @@ class GaudiSolver(TopOptSolver):
     def get_result(self) -> Result:
         return self.results.get()
 
-    def optimize(self, x: np.ndarray) -> np.ndarray:
+    def optimize(self, x: numpy.ndarray) -> numpy.ndarray:
         final = super().optimize(x)
 
         self.results.put(Result(final, final.sum(), 0, True))
@@ -352,3 +354,33 @@ class GaudiMockedGUI(GUI):
 
     def __init__(self, problem, title=""):
         pass
+
+
+class Optimization:
+    def __init__(self, project: Project):
+        self.project = project
+
+        self.problem = ComplianceProblem(CustomBoundaryConditions(self.project.domain.dimensions.width,  self.project.domain.dimensions.height, self.project.boundary_conditions),
+                                         self.project.penalization,
+                                         self.project.domain.material_properties.density,
+                                         self.project.domain.material_properties.elasticity)
+
+        self.gui = GaudiMockedGUI(self.problem, None)
+
+        self.topopt_filter = DensityBasedFilter(
+            self.project.domain.dimensions.width, self.project.domain.dimensions.height, project.filter_index)
+
+        self.solver = GaudiSolver(
+            self.problem, self.project.domain.volume_fraction, self.topopt_filter, self.gui)
+
+        self.identifier = hex(int(time.time() * 1000))[2:]
+
+    def get_result(self) -> Result:
+        return self.solver.get_result()
+
+    def optimize(self):
+        x = self.project.domain.volume_fraction * \
+            numpy.ones(self.project.domain.dimensions.width *
+                       self.project.domain.dimensions.height, dtype=float)
+
+        self.solver.optimize(x)
